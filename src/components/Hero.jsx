a8d71d91eb19e8b7498c031d6a6b14e0e4f3541a
Rohith__3330 <krohithmanikanta3@gmail.com>
2023-07-23 22:13:24 +0530

    final

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,25 +20,6 @@ const Hero = () => {
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>I devolep Machine learning, Deep learning  <br className='sm:block hidden'/>and Artificial intelligence <br className='sm:block hidden'/>models, Web applications <br className='sm:block hidden'/>and user interfaces </p>
         </div>
       </div>
-
-      <br/>
-      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
-        <a href='#about'>
-          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
-            <motion.div
-              animate={{
-                y: [0, 24, 0],
-              }}
-              transition={{
-                duration: 1.5,
-                repeat: Infinity,
-                repeatType: "loop",
-              }}
-              className='w-3 h-3 rounded-full bg-secondary mb-1'
-            />
-          </div>
-        </a>
-      </div>
     </section>
   );
 };
